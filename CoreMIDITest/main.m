ed74ab5337f31997561005841dcec155e4cc02df
Daniel Song <danielsong@daniels-macbook-pro.local>
2016-03-23 17:06:35 -0700

    Initial Commit

---
+++ CoreMIDITest/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  CoreMIDITest
+//
+//  Created by Daniel Song on 3/23/16.
+//  Copyright © 2016 Daniel Song. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
